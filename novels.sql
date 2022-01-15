377de54e82d02590bd6782d8ec3dd4d5873bc1ed
Marnie Kelly <marnie.kelly.123@gmail.com>
2022-01-15 14:50:45 -0500

    fixing typos

--- novels.sql
+++ novels.sql
@@ -2,6 +2,9 @@ CREATE DATABASE novels;
 
 CREATE USER 'novels'@'localhost' IDENTIFIED WITH mysql_native_password
 
-GRANT ALL ON heroes.* TO 'novels'@'localhost';
+GRANT ALL ON novels.* TO 'novels'@'localhost';
 
+CREATE DATABASE great_novels;
+
+USE great_novels;
 
